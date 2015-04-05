639f17e4e34fe10250035d76868722cc5acf044c
Brent Chuang <burennto@gmail.com>
2015-04-05 22:12:46 +1000

    Add Movie validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+
+  validates :title, presence: true,
+                    uniqueness: { case_sensitive: false }
+
+  validates :budget, :box_office, numericality: { only_integer: true }
+
 end

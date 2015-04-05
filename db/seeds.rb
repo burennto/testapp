4a9337f1bb3d375ac0437b550f7dcb294ff92852
Brent Chuang <burennto@gmail.com>
2015-04-05 22:13:23 +1000

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Movie.create(title: 'Iron Man',                            release_date: Date.new(2008, 5, 2),  budget: 150_000_000, box_office: 585_100_000)
+Movie.create(title: 'Iron Man 2',                          release_date: Date.new(2010, 5, 7),  budget: 200_000_000, box_office: 623_900_000)
+Movie.create(title: 'Iron Man 3',                          release_date: Date.new(2013, 5, 3),  budget: 200_000_000, box_office: 1_200_000_000)
+
+Movie.create(title: 'Thor',                                release_date: Date.new(2011, 5, 6),  budget: 150_000_000, box_office: 449_300_000)
+Movie.create(title: 'Thor: The Dark World',                release_date: Date.new(2013, 11, 8), budget: 170_000_000, box_office: 644_800_000)
+
+Movie.create(title: 'Captain America: The First Avenger',  release_date: Date.new(2011, 7, 22), budget: 140_000_000, box_office: 370_600_000)
+Movie.create(title: 'Captain America: The Winter Soldier', release_date: Date.new(2014, 4, 4),  budget: 170_000_000, box_office: 714_800_000)
+
+Movie.create(title: 'The Incredible Hulk',                 release_date: Date.new(2008, 6, 13), budget: 150_000_000, box_office: 263_400_000)
+Movie.create(title: 'Guardians of the Galaxy',             release_date: Date.new(2014, 8, 1),  budget: 195_900_000, box_office: 774_200_000)
+
+Movie.create(title: 'The Avengers',                        release_date: Date.new(2012, 5, 4),  budget: 220_000_000, box_office: 1_518_000_000)
